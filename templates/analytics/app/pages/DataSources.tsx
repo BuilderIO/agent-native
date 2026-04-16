556f41bdafa876f27f0b126e225623bce739e80a
Steve Sewell <steve@builder.io>
2026-04-16 14:34:28 -0700

    analytics: shrink-0 on data-source card icons so they don't squish

--- templates/analytics/app/pages/DataSources.tsx
+++ templates/analytics/app/pages/DataSources.tsx
@@ -539,7 +539,7 @@ function DataSourceCard({
         <CardHeader className="pb-3">
           <div className="flex items-center justify-between gap-6">
             <div className="flex items-center gap-3">
-              <div className="flex h-9 w-9 items-center justify-center rounded-lg bg-primary/10 text-primary">
+              <div className="flex h-9 w-9 shrink-0 items-center justify-center rounded-lg bg-primary/10 text-primary">
                 <Icon className="h-5 w-5" />
               </div>
               <div>
