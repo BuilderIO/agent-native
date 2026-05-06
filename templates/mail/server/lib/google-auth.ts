7ea64a52cc34d4df3b5c369285f696dbefe98988
Steve Sewell <steve@builder.io>
2026-05-06 15:08:24 -0400

    feat(mail): add Gmail filter API + settings.basic scope

--- templates/mail/server/lib/google-auth.ts
+++ templates/mail/server/lib/google-auth.ts
@@ -29,6 +29,7 @@ const SCOPES = [
   "https://www.googleapis.com/auth/gmail.readonly",
   "https://www.googleapis.com/auth/gmail.send",
   "https://www.googleapis.com/auth/gmail.modify",
+  "https://www.googleapis.com/auth/gmail.settings.basic",
   "https://www.googleapis.com/auth/userinfo.profile",
   "https://www.googleapis.com/auth/contacts.readonly",
   "https://www.googleapis.com/auth/contacts.other.readonly",
