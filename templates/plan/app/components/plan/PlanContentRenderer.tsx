af7f6a8dd1d0035677090de39357b32d3bba4272
Steve Sewell <steve@builder.io>
2026-06-22 07:58:56 -0700

    fix(plan): include annotated code in wide layout

--- templates/plan/app/components/plan/PlanContentRenderer.tsx
+++ templates/plan/app/components/plan/PlanContentRenderer.tsx
@@ -936,29 +936,30 @@ function renderFlowBlockFrame(block: PlanBlock, node: ReactNode) {
 function isWideLayoutBlock(block: PlanBlock): boolean {
   switch (block.type) {
     case "diff":
+    case "annotated-code":
       return true;
     case "tabs":
       return (
         block.data.orientation === "vertical" ||
-        block.data.tabs.some((tab) => blocksContainDiff(tab.blocks))
+        block.data.tabs.some((tab) => blocksContainDiffLike(tab.blocks))
       );
     default:
       return false;
   }
 }
 
-function blocksContainDiff(blocks: PlanBlock[]): boolean {
-  return blocks.some(blockContainsDiff);
+function blocksContainDiffLike(blocks: PlanBlock[]): boolean {
+  return blocks.some(blockContainsDiffLike);
 }
 
-function blockContainsDiff(block: PlanBlock): boolean {
-  if (block.type === "diff") return true;
+function blockContainsDiffLike(block: PlanBlock): boolean {
+  if (block.type === "diff" || block.type === "annotated-code") return true;
   if (block.type === "tabs") {
-    return block.data.tabs.some((tab) => blocksContainDiff(tab.blocks));
+    return block.data.tabs.some((tab) => blocksContainDiffLike(tab.blocks));
   }
   if (block.type === "columns") {
     return block.data.columns.some((column) =>
-      blocksContainDiff(column.blocks),
+      blocksContainDiffLike(column.blocks),
     );
   }
   return false;
