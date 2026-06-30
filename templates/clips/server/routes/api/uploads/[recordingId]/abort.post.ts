f44035fc0378badb02cfab7fced4ef67a04a4ef6
Milos Petrovic <milos@builder.io>
2026-06-30 18:41:49 +0200

    fix

--- templates/clips/server/routes/api/uploads/[recordingId]/abort.post.ts
+++ templates/clips/server/routes/api/uploads/[recordingId]/abort.post.ts
@@ -24,7 +24,7 @@ import {
   getEventOwnerContext,
   ownerEmailMatches,
 } from "../../../../lib/recordings.js";
-import { deleteResumableSession } from "../../../lib/resumable-session.js";
+import { deleteResumableSession } from "../../../../lib/resumable-session.js";
 
 export default defineEventHandler(async (event: H3Event) => {
   const recordingId = getRouterParam(event, "recordingId");
