a287c4bd21052811a0c007c1710e159dad606293
Steve Sewell <steve@builder.io>
2026-08-19 20:49:14 -0700

    chore: publish branch work in .github/workflows, scripts/guard-netlify-prebuilt-workflow.test.ts, scripts/guard-netlify-prebuilt-workflow.ts, … (4 files)

--- scripts/guard-netlify-prebuilt-workflow.test.ts
+++ scripts/guard-netlify-prebuilt-workflow.test.ts
@@ -7,7 +7,7 @@ import { parse } from "yaml";
 import {
   PRODUCTION_SITE_GROUP,
   validateProductionSiteConcurrency,
-} from "./netlify-production-concurrency.ts";
+} from "./guard-netlify-prebuilt-workflow.ts";
 
 type Workflow = Record<string, unknown>;
 
