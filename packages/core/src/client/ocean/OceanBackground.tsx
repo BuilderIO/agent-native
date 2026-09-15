e5995bd948b3efa798a0fdb1503762902ea6c590
Steve Sewell <steve@builder.io>
2026-09-15 13:11:11 -0700

    fix: import React for chat server rendering

--- packages/core/src/client/ocean/OceanBackground.tsx
+++ packages/core/src/client/ocean/OceanBackground.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 
 import { StarfieldBackground } from "../StarfieldBackground.js";
 import { HeroOceanBackground } from "./hero-ocean-background.js";
