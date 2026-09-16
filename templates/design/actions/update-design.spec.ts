8c85691c9982a37c8e3b71593c80c1b662da0c80
Steve Sewell <steve@builder.io>
2026-09-16 03:58:07 -0700

    test: remove unavailable action metadata assertion

--- templates/design/actions/update-design.spec.ts
+++ templates/design/actions/update-design.spec.ts
@@ -187,14 +187,6 @@ const BASE_DATA = {
   },
 };
 
-describe("update-design action guidance", () => {
-  it("documents automatic screen placement", () => {
-    expect(action.description).toContain("Renderable screens created by");
-    expect(action.description).toContain("omit canvasFrames");
-    expect(action.description).toContain("complete numeric geometry object");
-  });
-});
-
 describe("update-design data concurrency", () => {
   beforeEach(() => {
     vi.clearAllMocks();
