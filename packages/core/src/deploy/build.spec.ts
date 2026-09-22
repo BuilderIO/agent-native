ca3352d0e1a447fc02836ea6c3f5413d59840ff5
Steve Sewell <steve@builder.io>
2026-09-22 01:20:47 -0700

    test: cover multiline SSR re-exports

--- packages/core/src/deploy/build.spec.ts
+++ packages/core/src/deploy/build.spec.ts
@@ -2699,7 +2699,7 @@ describe("copyInstalledExternalSsrPackages", () => {
     expect(fs.existsSync(path.join(serverDir, "node_modules"))).toBe(false);
     fs.writeFileSync(
       path.join(serverDir, "chunk.mjs"),
-      'const react = require(`react`); export { Link } from "react-router"; export * from "@tanstack/react-query"; export { react };',
+      'const react = require(`react`);\nexport { Link } from "react-router";\nexport * from "@tanstack/react-query";\nexport { react };',
     );
 
     expect(
